e908bc7a4635cf31353241ae18c04a0a03d12bb2
y1555 <yatsok.vokiret17@mail.ru>
2025-02-20 18:51:32 +0300

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,4 +1,14 @@
 const fs = require("fs");
-  fs.stat("any.txt", (err, stats) => {
+
+  fs.writeFile("any2.txt", "anything", (err) => {
+    console.log(err);})
+  fs.readFile("any2.txt", (err, buf) => {
+    if(error){
+      return console.log(error)
+    }
+    console.log(buf);
+  });
+  fs.stat("any2.txt", (err, stats) => {
     console.log(stats);
   });
+  fs.unlink("any2.txt", (err) => {if(err) console.log(err)});
